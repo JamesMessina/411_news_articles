fd3ae7efd2e7fcdddf30175eb5efe2790f51d677
James Messina <jmessina17@gmail.com>
2021-12-30 11:08:55 -0600

    changed class to functional component

--- src/components/List.js
+++ src/components/List.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
+import React, {useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
-
-class List extends Component {
+function List(props) {
 
     
-    componentDidMount(){
-        this.props.fetchBands()
-    }
+    useEffect(() =>{
+        console.log("effect called")
+        props.fetchBands()
+    },[]);
 
-    render() {
-        return (
-            <div>
-                {this.props.bands.map((b, idx) => {
-                    return (
-                        <div key={idx}>
-                            <h1>{b.band_name}</h1>
-                            <p>{b.currentMembers}</p>
-                            <h6>{b.yearsActive}</h6>
-                            <hr></hr>
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Button variant="text">Text</Button>
+            {props.bands.map((b, idx) => {
+                return (
+                    <div key={idx}>
+                        <h1>{b.band_name}</h1>
+                        <p>{b.currentMembers}</p>
+                        <h6>{b.yearsActive}</h6>
+                        <hr></hr>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default List
